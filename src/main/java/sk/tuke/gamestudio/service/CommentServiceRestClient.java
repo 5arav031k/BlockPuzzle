ae3b64c3b4f6a3ac959d4f95cbac7ab0064afb6c
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-04-10 15:29:13 +0200

    final version 3 (jpa & rest api)

--- src/main/java/sk/tuke/gamestudio/service/CommentServiceRestClient.java
+++ src/main/java/sk/tuke/gamestudio/service/CommentServiceRestClient.java
@@ -1,6 +1,7 @@
 package sk.tuke.gamestudio.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import sk.tuke.gamestudio.entity.Comment;
 
@@ -8,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+@Service
 public class CommentServiceRestClient implements CommentService{
     private final String url = "http://localhost:8080/api/comment";
 
--- src/main/java/sk/tuke/gamestudio/service/LevelServiceRestClient.java
+++ src/main/java/sk/tuke/gamestudio/service/LevelServiceRestClient.java
@@ -1,9 +1,11 @@
 package sk.tuke.gamestudio.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import sk.tuke.gamestudio.entity.Level;
 
+@Service
 public class LevelServiceRestClient implements LevelService {
     private final String url = "http://localhost:8080/api/level";
 
